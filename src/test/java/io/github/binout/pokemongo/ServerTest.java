6f08e085f11a0bdd35e67837b6829243574bc620
Benoit Prioux <benoit.prioux@gmail.com>
2016-08-31 09:40:42 +0200

    add integration test for all pokedex entry

--- src/test/java/io/github/binout/pokemongo/ServerTest.java
+++ src/test/java/io/github/binout/pokemongo/ServerTest.java
@@ -1,6 +1,7 @@
 package io.github.binout.pokemongo;
 
 import com.github.kevinsawicki.http.HttpRequest;
+import io.github.binout.pokemongo.domain.Pokedex;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -27,5 +28,13 @@ public class ServerTest {
     @Test
     public void should_display_home() {
         assertThat(HttpRequest.get(baseUrl).ok()).isTrue();
+    }
+
+    @Test
+    public void should_rate_all_pokemons() {
+        Pokedex.get().allIds().forEach(id -> {
+            String json = "{\"id\": " + id.value() + ", \"team\": \"RED\", \"trainer\": \"Sacha\", \"cp\": 0, \"hp\": 0, \"dust\": 200}";
+            assertThat(HttpRequest.post(baseUrl + "/pokemon-rates").contentType("application/json").send(json.getBytes()).ok()).isTrue();
+        });
     }
 }
